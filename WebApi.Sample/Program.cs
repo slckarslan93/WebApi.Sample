5dc6bb8fb2815d5bb20deac3183e84c5658fce04
Selcuk <slckarslan93@gmail.com>
2025-03-26 11:37:03 +0300

    Add OpenAPI and Scalar API support
    Updated `Program.cs` to include `Scalar.AspNetCore` and register services for OpenAPI and Scalar API. Enhanced middleware to map OpenAPI and Scalar API references and enable routing for controllers.
    
    Added package references for `Microsoft.AspNetCore.OpenApi` version `9.0.3` and `Scalar.AspNetCore` version `2.1.1` in `WebApi.Sample.csproj`.

--- WebApi.Sample/Program.cs
+++ WebApi.Sample/Program.cs
@@ -1,9 +1,11 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Service Registration
 
 builder.Services.AddControllers(); //Api controller larýný ekleyebilmek için gereklidir. MVC Views desteði yoktur mvc controller için AddControllersWithViews() (Hem api hem MVC View desteðini ekler) service kaydýný yapmalýsýn. AddRazorPages() sadece Razor Pages desteði ekler.
-
+builder.Services.AddOpenApi();
 
 
 
@@ -12,6 +14,9 @@ builder.Services.AddControllers(); //Api controller larýný ekleyebilmek için ger
 var app = builder.Build();
 
 //Middlewares
+
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.MapControllers(); //AddControllers(); ile eklenen controller larý kullanabilmek (yönlendirilmesi (routing)) için gereklidir.
 
