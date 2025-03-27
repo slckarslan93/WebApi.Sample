ec723c4d67e196733753c35a0186d425da598bdb
Selcuk <slckarslan93@gmail.com>
2025-03-27 12:01:41 +0300

    Add ExceptionHandler for improved error handling
    Introduces the `ExceptionHandler` class in the `WebApi.Sample` namespace, implementing the `IExceptionHandler` interface. The `TryHandleAsync` method sets the HTTP response status to 400 and writes the exception message, enhancing the application's error handling capabilities.

---
+++ WebApi.Sample/ExceptionHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace WebApi.Sample
+{
+    public class ExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            httpContext.Response.StatusCode = 400;
+            await httpContext.Response.WriteAsync(exception.Message);
+
+            return true;
+        }
+    }
+}
